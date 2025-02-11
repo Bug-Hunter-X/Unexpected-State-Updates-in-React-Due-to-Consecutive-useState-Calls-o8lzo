0e4f9971ce39ea732191db770f6c4bf44ef9c449
BugHunter-X <askzenai@gmail.com>
2025-02-11 02:04:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,22 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect runs after every render
+    console.log('Count updated:', count);
+  }, [count]); 
+
+  const handleClick = () => {
+    setCount(prevCount => prevCount + 1);
+    setCount(prevCount => prevCount + 1); // This line is problematic
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  );
+}
+```
